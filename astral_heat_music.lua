426de0b9e33667442604bbe55792aad96a859d3e
dionednd <103206185+dionednd@users.noreply.github.com>
2025-12-21 12:01:23 +0800

    Update Astral Heat BGM Player to support multiple players

--- astral_heat_music.lua
+++ astral_heat_music.lua
@@ -1,20 +1,25 @@
 -- Astral Heat BGM Player for OHMSBY Style Characters
--- v 0.0.2
+-- v 0.0.3
 -- Commissioned by SkeleJ64
 
-AstralHeatBGMPlayed = false
+AstralHeatBGMPlayed = {}
 
 function f_AstralHeatBGM()
-	player(teamleader()) -- makes the module also work for tag team mode
-	if roundstate() == 2 and var(20) == 1 and not AstralHeatBGMPlayed then
-		AstralHeatBGMPlayed = true
-		playBgm({
-			source = "charparams.astral",
-			interrupt = true,
-		})
-	else
-		AstralHeatBGMPlayed = false
+	if roundstate() ~= 2 then return end
+	local nump = numenemy() + numpartner() + 1
+	for p = 1, nump do
+		player(p)
+		if var(20) == 1
+		and not AstralHeatBGMPlayed[p]
+		and playerno() == teamleader() then
+			AstralHeatBGMPlayed[p] = true
+			playBgm({
+				source = "charparams.astral",
+				interrupt = true,
+			})
+		elseif var(20) ~= 1 then
+			AstralHeatBGMPlayed[p] = false
+		end
 	end
 end
-
 hook.add("loop", "AstralHeatBGM", f_AstralHeatBGM)
