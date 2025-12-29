b1e6afeb4f3d9e3007b6f63715b2de0327cd3dc5
dionednd <103206185+dionednd@users.noreply.github.com>
2025-12-29 23:52:16 +0800

    Enhance BGM selection for rival characters
    Updated the Astral Heat BGM player to select rival music based on the opponent's character.

--- astral_heat_music.lua
+++ astral_heat_music.lua
@@ -1,24 +1,48 @@
--- Astral Heat BGM Player for OHMSBY Style Characters
--- v 0.0.3a
+-- Astral Heat BGM Player
+-- v0.0.4
 -- Commissioned by SkeleJ64
 
 AstralHeatBGMPlayed = {}
 
 function f_AstralHeatBGM()
 	if roundstate() ~= 2 then return end
+	
 	for p = 1, 8 do
 		player(p)
-		if var(20) == 1
-		and not AstralHeatBGMPlayed[p]
+
+		if var(20) == 1 
+		and not AstralHeatBGMPlayed[p] 
 		and playerno() == teamleader() then
+			
 			AstralHeatBGMPlayed[p] = true
+			
+			local oppName = ""
+			enemy(0)
+			oppName=name()
+			player(p)
+
+			local track = "charparams.astral"
+			for i = 1, 9 do
+				local key = "rival" .. i .. "name"
+				local riv = main.t_selChars[start.c[p].selRef + 1][key]
+				if riv ~= "" and riv == oppName then
+					local rMusic = "charparams.rival" .. i
+					track = rMusic
+					break
+				end
+			end
+
+			if track == "" then track = "charparams.astral" end
+
 			playBgm({
-				source = "charparams.astral",
+				source = track,
 				interrupt = true,
 			})
+			
 		elseif var(20) ~= 1 then
 			AstralHeatBGMPlayed[p] = false
 		end
 	end
 end
+
 hook.add("loop", "AstralHeatBGM", f_AstralHeatBGM)
