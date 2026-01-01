f81b4a44fac5bed1967c309c00cc05febf6301b3
dionednd <103206185+dionednd@users.noreply.github.com>
2026-01-01 22:50:49 +0800

    Upgrade Astral Heat BGM Player to v0.0.4d
    Updated the Astral Heat BGM Player to version 0.0.4d with enhancements to prevent crashes in demo mode and added support for additional authors.

--- astral_heat_music.lua
+++ astral_heat_music.lua
@@ -1,9 +1,10 @@
 -- Astral Heat BGM Player
--- v0.0.4c
+-- v0.0.4d
 -- Commissioned by SkeleJ64
 
 local AstralHeatBGMPlayed = {}
-
+local oppName = {}
+local rival = {}
 local Authors = {
 	["OHMSBY"] = true,
 	["Ichida"] = true,
@@ -14,42 +15,45 @@ local Authors = {
 	["ZolidSone"] = true,
 	["JoeyS"] = true,
 	["dionednd"] = true,
+	["Rouuuu"] = true,
+	["OWO"] = true,
+	["Forla"] = true,
 }
 
 
 function f_AstralHeatBGM()
-	-- if roundstate() ~= 2 then return end
+	if gamemode() == "demo" then return end -- to prevent game from crashing, we do not load the module during demo mode.
 	for side = 1, 2 do
-		for _, v in pairs(start.p[side].t_selected) do
-			if player(v.pn) then
+		for member, v in pairs(start.p[side].t_selected) do
+			local pn = 2 * (member - 1) + side
+			if player(pn) then
+
 				local author = authorname()
 				if var(20) == 1 
-				and not AstralHeatBGMPlayed[v.pn] 
+				and not AstralHeatBGMPlayed[pn] 
 				and playerno() == teamleader()
-				and Authors[author] then
+				and Authors[author] 
+				and roundstate() == 2 then
 				
-					AstralHeatBGMPlayed[v.pn] = true
-			
-					local oppName = ""
-					if enemynear(0) then
-						oppName=name()
-					end
-					player(v.pn)
+					AstralHeatBGMPlayed[pn] = true
+
+					enemynear(0)
+					oppName[pn]=name()
+					player(pn)
 
 					local track = "charparams.astral"
-
-					for i = 1, 32 do
-						local key = "rival" .. i .. "name"
-						local rival = start.f_getCharData(v.ref)[key]
-						
-						if rival == oppName then
-							local rMusic = "charparams.rival" .. i
-							track = rMusic
-							break
+					if teammode() ~= "turns" then -- disable for turns mode. will come back to this when i find a way how to fix rival themes for turns mode.
+												-- the issue is that in turns mode, player numbers are always 1 and 2, so when fetching the rivalname, it always returns p1 and p2's rivalname values.
+						for i = 1, 32 do
+							local key = "rival" .. i .. "name"
+							rival[pn] = start.f_getCharData(v.ref)[key]
+							if rival[pn] and rival[pn] == oppName[pn] then
+								local rMusic = "charparams.rival" .. i
+								track = rMusic
+								break
+							end
 						end
 					end
-
-					if track == "" then track = "charparams.astral" end
 
 					playBgm({
 						source = track,
@@ -57,7 +61,7 @@ function f_AstralHeatBGM()
 					})
 			
 				elseif var(20) ~= 1 then
-					AstralHeatBGMPlayed[v.pn] = false
+					AstralHeatBGMPlayed[pn] = false
 				end
 			end
 		end
